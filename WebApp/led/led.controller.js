e87a62c90c409e2209d9acc23fe17e045205ec2d
Nicolás Jiménez García <nicolas.j2007@gmail.com>
2017-06-16 01:08:09 -0600

    web app sends custom encoded emojis to api ready-to-use

--- WebApp/led/led.controller.js
+++ WebApp/led/led.controller.js
@@ -2,14 +2,45 @@ angular.module('app')
     .controller('LedController', [ 'LedService','FlashService', function (LedService, FlashService) {
         var vm = this;
 
-
-
         vm.sendMessage = sendMessage;
-
+        vm.smiley = smiley;
+        vm.flat = flat;
+        vm.cute = cute;
+        vm.annoyed = annoyed;
+        vm.heart = heart;
+        vm.love = love
+        vm.winkle = winkle;
+        vm.meh =meh;
+        vm.flat2 = flat2;
+        vm.surprised = surprised;
 
         function sendMessage(){
 
-            var send = {mensaje: vm.input}
+            var len = vm.input.length; 
+            if (len>20){
+                FlashService.Error("Mensaje no debe ser mayor a 20 caracteres");
+                return;
+            }
+
+            var message = vm.input;
+
+            for (var i = 0, len = message.length; i < len; i++) {
+
+                message = message.replace("😁", "!");
+                message = message.replace("😐", "@");
+                message = message.replace("😊", "#");
+                message = message.replace("😒", "$");
+                message = message.replace("❤️", "%");
+                message = message.replace("😍", "^");
+                message = message.replace("😉", "&");
+                message = message.replace("😕", "*");
+                message = message.replace("😑", "(");
+                message = message.replace("😮", ")");
+
+            }
+            var send = {mensaje: message};
+            console.log(send);
+
             LedService.SendMessage(send)
                 .then(function(response){
 
@@ -28,4 +59,34 @@ angular.module('app')
             });
         }
 
+        function smiley(){
+            vm.input = vm.input.concat("😁");
+        }
+        function flat(){
+            vm.input = vm.input.concat("😐");
+        }
+        function cute(){
+            vm.input = vm.input.concat("😊");
+        }
+        function annoyed(){
+            vm.input = vm.input.concat("😒");
+        }
+        function heart(){
+            vm.input = vm.input.concat("❤️");
+        }
+        function love(){
+            vm.input = vm.input.concat("😍");
+        }
+        function winkle(){
+            vm.input = vm.input.concat("😉");
+        }
+        function meh(){
+            vm.input = vm.input.concat("😕");
+        }
+        function flat2(){
+            vm.input = vm.input.concat("😑");
+        }
+        function surprised(){
+            vm.input = vm.input.concat("😮");
+        }
     }]);
