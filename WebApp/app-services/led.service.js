459f8f88f997f59e04fae77e0d02140071fbd222
Nicolás Jiménez García <nicolas.j2007@gmail.com>
2017-06-16 14:42:54 -0600

    ip update for webapp

--- WebApp/app-services/led.service.js
+++ WebApp/app-services/led.service.js
@@ -13,7 +13,7 @@
         function SendMessage(data){
             var response= $http({
                 method:"post",
-                url: "http://192.168.100.5:3000/send",
+                url: "http://172.16.119.129:3000/send",
                 data: data
             });
             return response;
